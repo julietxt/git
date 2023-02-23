f68122831899a99c514fa732248b8aaab0d6bb9c
Julieta Vera <julietaojeda@gmail.com>
2023-02-23 15:15:04 -0300

    agrego Persona al script

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
-from modulo import suma, resta
+from modulo import suma, resta, Persona
 
 print("Hello everyone")
 
 print(f"La suma de 1+2= {suma(1,2)}")
 
 print(f"La resta de 2-1= {resta(2,1)}")
+
+persona_1= Persona()
